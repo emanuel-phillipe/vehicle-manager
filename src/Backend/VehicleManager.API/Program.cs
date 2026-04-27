133c2f10e0c656bb1d3a8af2afbf1ff1e1fc0d66
emanuel-phillipe <emanuelphillipe1706@gmail.com>
2026-04-27 15:42:41 -0300

    CHORE: Making URLs lowercased

--- src/Backend/VehicleManager.API/Program.cs
+++ src/Backend/VehicleManager.API/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Lowercased Urls
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 var app = builder.Build();
 
