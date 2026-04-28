a5f63bb57ed851ac112a7c5ce56568ef8eea9e07
emanuel-phillipe <emanuelphillipe1706@gmail.com>
2026-04-28 12:07:46 -0300

    FEAT: Starting Point of Exceptions And it's Messages

--- src/Backend/VehicleManager.Application/Usecases/User/Register/RegisterUserValidator.cs
+++ src/Backend/VehicleManager.Application/Usecases/User/Register/RegisterUserValidator.cs
@@ -1,20 +1,21 @@
 using FluentValidation;
 using VehicleManager.Communication.Requests;
+using VehicleManager.Exceptions;
 
 namespace VehicleManager.Application.Usecases.User.Register;
 
+
 public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson> 
 {
-    public  RegisterUserValidator()
+    public RegisterUserValidator()
     {
-        RuleFor(user => user.FullName).NotEmpty();
-        RuleFor(user => user.Cpf.Length).Equal(11);
-        RuleFor(user => user.Email).EmailAddress();
-        RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(8);
-        RuleFor(user => user.Phone.Length).Equal(14);
-        RuleFor(user => user.Role).NotEmpty();
-        RuleFor(user => user.CnhCategories).NotEmpty();
-        RuleFor(user => user.CnhDueDate).NotEmpty();
-        RuleFor(user => user.CnhNum.Length).Equal(9);
-    }
-}
+        RuleFor(user => user.FullName).NotNull().NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY);
+        RuleFor(user => user.Cpf.Length).NotNull().Equal(11).WithMessage("Cpf should be 11 digits");
+        RuleFor(user => user.Email).EmailAddress().WithMessage("Email is required");
+        RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(8).WithMessage("Password should be greater than or equal to 8 digits");
+        RuleFor(user => user.Phone.Length).Equal(14).WithMessage("Phone number should be equal to 14 digits");
+        RuleFor(user => user.Role).NotEmpty().WithMessage("Role is required");
+        RuleFor(user => user.CnhCategories).NotEmpty().WithMessage("CNH categories is required");
+        RuleFor(user => user.CnhDueDate).NotEmpty().WithMessage("CNH due date is required");
+        RuleFor(user => user.CnhNum.Length).Equal(9).WithMessage("CNH number should be 9 digits");
+    } }
