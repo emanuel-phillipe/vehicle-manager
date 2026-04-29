18ce5eac4cfb7be08b0dc76adf9e456f68c19425
emanuel-phillipe <emanuelphillipe1706@gmail.com>
2026-04-29 15:11:27 -0300

    FEAT: Adding AutoMapping Foundation

---
+++ src/Backend/VehicleManager.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using VehicleManager.Communication.Requests;
+
+namespace VehicleManager.Application.Services.AutoMapper;
+
+public class AutoMapping:Profile
+{
+    public AutoMapping()
+    {
+        RequestToDomain();
+    }
+
+    private void RequestToDomain()
+    {
+        CreateMap<RequestRegisterUserJson, Domain.Entities.User>()
+            .ForMember(user => user.Password, opt => opt.Ignore());
+    }
+}
