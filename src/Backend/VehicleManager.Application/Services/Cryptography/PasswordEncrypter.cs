89a0900f56f07c1e93cf0611ed2a346f8240356c
emanuel-phillipe <emanuelphillipe1706@gmail.com>
2026-04-29 15:46:10 -0300

    FEAT: Adding Password Encryption Foundations & Code Commenting

---
+++ src/Backend/VehicleManager.Application/Services/Cryptography/PasswordEncrypter.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace VehicleManager.Application.Services.Cryptography;
+
+public class PasswordEncrypter
+{
+    public string Encrypt(string password)
+    {
+        var addKey = "ABC";
+        var newPassword = $"{password}{addKey}";
+        
+        var bytes = Encoding.UTF8.GetBytes(newPassword);
+        var hashBytes = SHA512.HashData(bytes);
+        
+        return StringBytes(hashBytes);
+    }
+    
+    // Função para converter Array de bytes em String (Fonte: Welisson Arley, em 29/04/2026)
+    private static string StringBytes(byte[] bytes)
+    {
+        var sb = new StringBuilder();
+        foreach (byte b in bytes)
+        {
+            var hex = b.ToString("x2");
+            sb.Append(hex);
+        }
+        
+        return sb.ToString();
+    }
+}
--- src/Backend/VehicleManager.Application/Usecases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/VehicleManager.Application/Usecases/User/Register/RegisterUserUseCase.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Logging.Abstractions;
 using VehicleManager.Application.Services.AutoMapper;
+using VehicleManager.Application.Services.Cryptography;
 using VehicleManager.Communication.Requests;
 using VehicleManager.Communication.Responses;
 
@@ -9,20 +10,27 @@ public class RegisterUserUseCase
 {
     public ResponseRegisterUserJson Execute(RequestRegisterUserJson request)
     {
-        // Validar body da request
-        Validate(request);
-
+        // OBS - INJEÇÃO DE DEPENDÊNCIAS NÃO INICIADA
+        //Inicialização de classes (Encriptação e AutoMapper)
+        var passwordCrypto = new PasswordEncrypter();
         var autoMapper = new AutoMapper.MapperConfiguration(options=>
         {
             options.AddProfile(new AutoMapping());
         }, NullLoggerFactory.Instance).CreateMapper();
+        
+        // Validação do request
+        Validate(request);
 
+        // Mapping da entidade
         var user = autoMapper.Map<Domain.Entities.User>(request);
+
+        //Criptografia da senha do usuário
+        user.Password = passwordCrypto.Encrypt(request.Password);
         
         return new ResponseRegisterUserJson()
         {
-            FullName = request.FullName,
-            Email = request.Email,
+            FullName = user.FullName,
+            Email = user.Password,
         };
     }
 
